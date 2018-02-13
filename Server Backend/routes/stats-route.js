e7b56fea3c3a0481947273a2124b9f897a96ddea
Landon Patmore <landon.patmore@gmail.com>
2018-02-12 19:44:50 -0500

    Added boilerplate files and structure

---
+++ Server Backend/routes/stats-route.js
@@ -0,0 +1,11 @@
+/**
+ * Imports the `express` and `router` modules
+ */
+const express = require("express");
+const router = express.Router();
+
+
+/**
+ * Exports this file so it can be used by other files.  Keep this at the bottom.
+ */
+module.exports = router;
