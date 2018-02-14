7d256c8e79e6f585e3e9cd2138603b31978804c8
Landon Patmore <landon.patmore@gmail.com>
2018-02-13 21:23:24 -0500

    added users array

--- Server Backend/routes/users-route.js
+++ Server Backend/routes/users-route.js
@@ -4,6 +4,11 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * Users array
+ */
+var users = [];
+
 
 /**
  * Exports this file so it can be used by other files.  Keep this at the bottom.
