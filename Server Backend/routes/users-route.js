f4f94e330e5df3080ab53717b6c89b383d493fd3
Landon Patmore <landon.patmore@gmail.com>
2018-02-14 02:01:32 -0500

    Added documentation

--- Server Backend/routes/users-route.js
+++ Server Backend/routes/users-route.js
@@ -9,15 +9,36 @@ const router = express.Router();
  */
 var users = [];
 
+/**
+ * Route that renders an HTML page for adding a user
+ */
 router.get("/addUserPage", function(req, res, next){
+
+    /**
+     * HTML page to be rendered
+     */
     res.render("addUser");
 });
 
+/**
+ * Route that renders an HTML page for viewing all users
+ */
 router.get("/usersPage", function(req, res, next){
+
+    /**
+     * HTML page to be rendered
+     */
     res.render("allUsers");
 });
 
+/**
+ * Route that returns all of the users
+ */
 router.get("/allUsers", function (req, res, next) {
+
+    /**
+     * Sends the user a JSON object containing all of the users
+     */
 	res.json({
 		all_users: users
 	});
@@ -39,19 +60,44 @@ router.post("/createUser", function (req, res, next) {
      * Add new user object to users array
      */
     users.push(newUser);
-
+    
+    /**
+     * Logs to the console what user was added
+     */
     console.log("User: " + newUser.userName + " added.")
 
+    /**
+     * Returns to the user a JSON object containing the new user
+     */
     res.json(newUser);
 });
 
+/**
+ * Route that deletes a user
+ */
 router.delete("/deleteUser", function(req, res, next){
+    /**
+     * The index of the user to be deleted
+     */
     const index = req.body.user;
+    /**
+     * The user to be deleted
+     */
     const user = users[index];
 
+    /**
+     * Removes the user from the list
+     */
     users.splice(user, 1);
 
+    /**
+     * Logs to the console what user was added
+     */
     console.log("User: "+ user.userName + " deleted.");
+
+    /**
+     * Sends back a status code of whether or not a user was successfully deleted or not
+     */
     res.sendStatus(users[index] !== user ? 200 : 400);
 });
 
