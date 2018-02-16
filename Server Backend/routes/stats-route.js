4953c931f2a0069f4e5ade1663aec704ab53cb1d
Gage Davidson <gaged97@gmail.com>
2018-02-16 15:19:20 -0500

    Added tentative endpoint stubs

--- Server Backend/routes/stats-route.js
+++ Server Backend/routes/stats-route.js
@@ -4,6 +4,33 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * This endpoint gets the collective score for green team
+ */
+router.get("/greenScore", function(req, res, next){
+    
+});
+
+/**
+ * This endpoint gets the collective score for gold team
+ */
+router.get("/goldScore", function(req, res, next){
+
+});
+
+/**
+ * This endpoint gets the largest word currently in play
+ */
+router.get("/largeWords", function(req, res, next){
+
+});
+
+/**
+ * This endpoint provides a list of top users
+ */
+router.get("/topPlayers", function(req, res, next){
+    
+});
 
 /**
  * Exports this file so it can be used by other files.  Keep this at the bottom.
