583f47356182ef87a2510f2dc88141e0eb9433b1
Landon Patmore <landon.patmore@gmail.com>
2018-02-17 23:05:27 -0500

    documentation

--- Server Backend/helpers/Tile.js
+++ Server Backend/helpers/Tile.js
@@ -1,6 +1,11 @@
 'use strict'
 
 class Tile {
+  /**
+   * @param {Number} x - x position
+   * @param {Number} y - y position
+   * @param {String} multiplier - the multiplier this Tile has
+   */
   constructor(x, y, multiplier) {
     this._x = x
     this._y = y
@@ -9,42 +14,72 @@ class Tile {
     this._letter = ''
   }
 
+  /**
+   * X Setter
+   */
   set x(x) {
     this._x = x
   }
 
+  /**
+   * Y Setter
+   */
   set y(y) {
     this._y = y
   }
 
+  /**
+   * Multiplier Setter
+   */
   set multiplier(multiplier) {
     this._multiplier = multiplier
   }
 
+  /**
+   * Letter Setter
+   */
   set letter(letter) {
     this._letter = letter
     this._letterPlaced = true
   }
 
+  /**
+   * X Getter
+   */
   get x() {
     return this._x
   }
 
+  /**
+   * Y Getter
+   */
   get y() {
     return this._y
   }
 
+  /**
+   * Multiplier Getter
+   */
   get multiplier() {
     return this._multiplier
   }
 
+  /**
+   * Letter placed getter
+   */
   get letterPlaced() {
     return this._letterPlaced
   }
 
+  /**
+   * Letter getter
+   */
   get letter() {
     return this._letter
   }
 }
 
+/**
+ * Exports this file so it can be used by other files.  Keep this at the bottom.
+ */
 module.exports = Tile
