9a3aebd57241cf5c017c10913de710ae8dead110
Landon Patmore <landon.patmore@gmail.com>
2018-02-17 22:51:38 -0500

    added documentation

--- Server Backend/helpers/Gameboard.js
+++ Server Backend/helpers/Gameboard.js
@@ -1,25 +1,44 @@
 'use strict'
+/**
+ * Imports the Tile class
+ */
 const Tile = require('../helpers/Tile')
 
 class Gameboard {
+  /**
+   * @param {Number} size - the size of the board for both height and width
+   */
   constructor(size) {
     this._size = size
     this._board = new Array(this._size)
     this._initialized = false
   }
 
+  /**
+   * Size setter
+   */
   set size(size) {
     this._size = size
   }
 
+  /**
+   * Size getter
+   */
   get size() {
     return this._size
   }
 
+  /**
+   * Initialized getter
+   */
   get initialized() {
     return this._initialized
   }
 
+  /**
+   * Creates a double array of Tiles
+   * If this method has already been run, it returns before it can recreate the double array
+   */
   init() {
     if (this._initialized) return
 
@@ -35,4 +54,7 @@ class Gameboard {
   }
 }
 
+/**
+ * Exports this file so it can be used by other files.  Keep this at the bottom.
+ */
 module.exports = Gameboard
