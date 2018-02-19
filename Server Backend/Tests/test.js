be6bd8780b7c17fbc53cab5b1cd6a3e38b6e16d6
Landon Patmore <landon.patmore@gmail.com>
2018-02-19 01:49:21 -0500

    Added more tests
    - added test to check horizontal word placement
    - added test to check vertical word placement
    - renamed Tile tests to have "should" in test name instead
    - added test to check if a letter was not placed

--- Server Backend/Tests/test.js
+++ Server Backend/Tests/test.js
@@ -17,10 +17,58 @@ test('Gameboard should be initialized', t => {
   t.true(g.initialized)
 })
 
+test('Gameboard should not be re-initialized', t => {
+  const g = new GB(15)
+
+  g.init()
+
+  t.true(g.init())
+})
+
 test('Gameboard size should be 15', t => {
   const g = new GB(15)
 
   t.deepEqual(g.size, 15)
+})
+
+test('Gameboard should place word "OSWEGO" horizontally from (2,2) to (7, 2)', t => {
+  const g = new GB(15)
+
+  g.init()
+
+  const word = 'OSWEGO'
+  const startX = 2
+  const startY = 2
+  const endX = 7
+  const endY = 2
+
+  g.placeWord({ x: startX, y: startY }, { x: endX, y: endY }, word)
+
+  for (let i = startX; i <= endX; i++) {
+    for (let j = startY; j <= endY; j++) {
+      t.deepEqual(g.board[j][i].letter.toUpperCase(), word[i - startX].toUpperCase())
+    }
+  }
+})
+
+test('Gameboard should place word "OSWEGO" vertically from (2,2) to (2, 7)', t => {
+  const g = new GB(15)
+
+  g.init()
+
+  const word = 'OSWEGO'
+  const startX = 2
+  const startY = 2
+  const endX = 2
+  const endY = 7
+
+  g.placeWord({ x: startX, y: startY }, { x: endX, y: endY }, word)
+
+  for (let i = startX; i <= endX; i++) {
+    for (let j = startY; j <= endY; j++) {
+      t.deepEqual(g.board[j][i].letter.toUpperCase(), word[j - startY].toUpperCase())
+    }
+  }
 })
 
 test('Tile should be created', t => {
@@ -55,10 +103,16 @@ test('Tile should be the letter "Q"', t => {
   t.deepEqual(tile.letter, 'Q')
 })
 
-test('Tile have a letter placed', t => {
+test('Tile should have a letter placed', t => {
   const tile = new Tile(1, 1, '0')
 
   tile.letter = 'Q'
 
   t.true(tile.letterPlaced)
+})
+
+test('Tile should not have a letter placed', t => {
+  const tile = new Tile(1, 1, '0')
+
+  t.false(tile.letterPlaced)
 })
