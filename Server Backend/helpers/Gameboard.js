9e08b6b211216f2f0c9ff4388f91e55c85431024
Landon Patmore <landon.patmore@gmail.com>
2018-02-20 21:21:10 -0500

    Added documentation and trimmed code
    - added word validation documentation
    - slimmed playWord()
    - slimmed validatePosition() : changed name as well

--- Server Backend/helpers/Gameboard.js
+++ Server Backend/helpers/Gameboard.js
@@ -64,25 +64,25 @@ class Gameboard {
   }
 
   /**
+   * Method that places a word on the baord
    * @param {Object} startCoords - object structured as such: {x: x, y: y}
    * @param {Object} endCoords - object structured as such: {x: x, y: y}
    * @param {String} word - word that will be placed on the board
    */
   placeWord(startCoords, endCoords, word) {
-    let tempBoard = _.cloneDeep(this.board)
+    const tempBoard = _.cloneDeep(this.board)
+    const val = this.validatePosition
+
     for (let i = startCoords.x; i <= endCoords.x; i++) {
       for (let j = startCoords.y; j <= endCoords.y; j++) {
-        if (startCoords.x === endCoords.x) {
-          if (!this.validateWordPlacement(tempBoard[j][i].letter, word[j - startCoords.y].toUpperCase())) {
-            return false
-          }
-          tempBoard[j][i].letter = word[j - startCoords.y].toUpperCase()
-        } else {
-          if (!this.validateWordPlacement(tempBoard[j][i].letter, word[i - startCoords.x].toUpperCase())) {
-            return false
-          }
-          tempBoard[j][i].letter = word[i - startCoords.x].toUpperCase()
+        let l = tempBoard[j][i].letter
+        let w = startCoords.x === endCoords.x ? word[j - startCoords.y].toUpperCase() : word[i - startCoords.x].toUpperCase()
+
+        if (!val(l, w)) {
+          return false
         }
+
+        tempBoard[j][i].letter = w
       }
     }
 
@@ -90,14 +90,13 @@ class Gameboard {
     return true
   }
 
-  validateWordPlacement(currentLetter, toBePlacedLetter) {
-    if (currentLetter === '.') {
-      return true
-    } else if (currentLetter === toBePlacedLetter) {
-      return true
-    }
-
-    return false
+  /**
+   * Validates whether a letter can be placed in the current position
+   * @param {String} currentLetter - the letter that is currently on the board
+   * @param {String} toBePlacedLetter - the letter that is to be placed on the board
+   */
+  validatePosition(currentLetter, toBePlacedLetter) {
+    return currentLetter === '.' || currentLetter === toBePlacedLetter
   }
 }
 
