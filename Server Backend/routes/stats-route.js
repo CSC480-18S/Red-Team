92a08de19715b189f413a19c4bad92ea1185c595
Gage Davidson <gaged97@gmail.com>
2018-02-22 20:07:37 -0500

    New stats endpoints

--- Server Backend/routes/stats-route.js
+++ Server Backend/routes/stats-route.js
@@ -5,51 +5,107 @@ const express = require('express')
 const router = express.Router()
 
 /**
- * This endpoint gets the collective score for green team.
+ * This endpoint gets the highest valued word played by the repsective team.
  */
-router.get('/gameScore', function(req, res, next) {
+router.get('/team/topValueWord', function(req, res, next) {
+    
+})
+
+/**
+ * This endpoint gets the score value of the highest valued word played by the respective team.
+ */
+router.get('/team/highestValue', function(req, res, next) {
 
 })
 
 /**
- * This endpoint gets the <x amount> most valuable words ever played.
+ * This endpoint gets the longest word played by the respective team.
  */
-router.get('/bestWords', function(req, res, next) {
+router.get('/team/longestWord', function(req, res, next) {
 
 })
 
 /**
- * This endpoint provides a list of the top <x amount> users.
+ * This endpoint gets the highest single game score for the respective team.
  */
-router.get('/topPlayers', function(req, res, next) {
+router.get('/team/highestSingleGameScore', function(req, res, next) {
 
 })
 
 /**
- * This endpoint provides the game score of the game containing the highest game score.
+ * This endpoint returns the most frequently played word by the respective team.
  */
-router.get('/highestGameScore', function(req, res, next) {
+router.get('/team/freqPlayedWord', function(req, res, next) {
 
 })
 
 /**
- * This endpoint provides the cumulative team scores.
+ * This endpoint gets the number of bonuses cumulatively played by the respective team.
  */
-router.get('/cumulativeTeamScores', function(req, res, next) {
+router.get('/team/amountBonusesUsed', function(req, res, next) {
 
 })
 
 /**
- * This endpoint provides a list of the <x amount> most-played words.
+ * This endpoint gets the cumulative game score for the respective team.
  */
-router.get('/mostPlayedWords', function(req, res, next) {
+router.get('/team/cumulativeScore', function(req, res, next) {
 
 })
 
 /**
- * This endpoint provides the total number of times each team has won.
+ * This endpoint gets the total number of wins for the respective team.
  */
-router.get('/totalTeamWins', function(req, res, next) {
+router.get('/team/winCount', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint gets the highest valued word played by the respective player.
+ */
+router.get('/player/topValueWord', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint gets the score value of the highest valued word played by the respective player.
+ */
+router.get('/player/highestValue', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint gets the longest word played by the respective player.
+ */
+router.get('/player/longestWord', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint gets the highest single game score for the respective player.
+ */
+router.get('/player/highestSingleGameScore', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint returns the most frequently played word by the respective player.
+ */
+router.get('/player/freqPlayedWord', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint gets the number of bonuses cumulatively played by the respective player.
+ */
+router.get('/player/amountBonusesUsed', function(req, res, next) {
+
+})
+
+/**
+ * This endpoint gets the cumulative score for the respective player.
+ */
+router.get('/player/totalScore', function(req, res, next) {
 
 })
 
