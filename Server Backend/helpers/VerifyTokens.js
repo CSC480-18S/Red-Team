206376525dcc01625e155ba52ff6708867176be0
Landon Patmore <landon.patmore@gmail.com>
2018-02-27 00:37:52 -0500

    changed error messages to reflect new pipeline

--- Server Backend/helpers/VerifyTokens.js
+++ Server Backend/helpers/VerifyTokens.js
@@ -16,10 +16,10 @@ router.use(cookieParser())
  */
 function verifyToken(req, res, next) {
   var token = req.cookies.token
-  if (!token) { return res.status(403).send({ auth: false, message: 'No token provided.' }) }
+  if (!token) { return res.status(400).json({code: 'A1', title: 'Auth error', desc: 'No token provided.'}) }
 
   jwt.verify(token, config.secret, function(err, decoded) {
-    if (err) { return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' }) }
+    if (err) { return res.status(500).json({code: 'A2', title: 'Auth error', desc: 'Failed to authenticate token.'}) }
 
     // if everything good, save to request for use in other routes
     req.userId = decoded.id
