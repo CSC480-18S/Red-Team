bed2c626812e1aa49ace45171287ec7f5bb3c96f
Landon Patmore <landon.patmore@gmail.com>
2018-03-02 00:16:22 -0500

    Update and fixes
    - word now has to be connected to other played words to be valid
    - added verification to routes
    - refactored playWord() method to use horizontal boolean so there isn't
    an extra check
    - gameboard can now communicate with the DB and respond back to the user
    after a DB request is made
    - tests added
    - cleaned up code
    - added documentation
    - small things

--- Server Backend/tests/test.js
+++ Server Backend/tests/test.js
@@ -1,5 +1,5 @@
-const GB = require('../helpers/Gameboard')
-const Tile = require('../helpers/Tile')
+const GB = require('../entities/Gameboard')
+const Tile = require('../entities/Tile')
 /**
  * Imports the lodash library
  */
@@ -7,11 +7,11 @@ const _ = require('lodash')
 
 describe('Gameboard tests', () => {
   it('Gameboard should be created', () => {
-    expect(new GB(11)).toBeTruthy()
+    expect(new GB()).toBeTruthy()
   })
 
   it('Gameboard should be initialized', () => {
-    const g = new GB(11)
+    const g = new GB()
 
     g.init()
 
@@ -19,7 +19,7 @@ describe('Gameboard tests', () => {
   })
 
   it('Gameboard should not be re-initialized', () => {
-    const g = new GB(11)
+    const g = new GB()
 
     g.init()
 
@@ -27,219 +27,240 @@ describe('Gameboard tests', () => {
   })
 
   it('Gameboard size should be 11', () => {
-    const g = new GB(11)
+    const g = new GB()
 
     expect(g.size).toBe(11)
   })
 
-  it('Gameboard should place word "OSWEGO" horizontally from (2,2) to (7, 2)', () => {
-    const g = new GB(11)
+  it('Gameboard should place word "OSWEGO" horizontally from (5,5) to (10, 5)', () => {
+    const g = new GB()
 
     g.init()
 
     const word = 'OSWEGO'
-    const x = 2
-    const y = 2
     const h = true
 
-    g.consumeInput(x, y, h, word)
+    g.placeWord({x: 5, y: 5}, {x: 10, y: 5}, word, h)
 
-    for (let i = x; i < word.length; i++) {
-      for (let j = y; j <= y; j++) {
-        expect(g.board[j][i].letter.toUpperCase()).toEqual(word[i - x].toUpperCase())
-      }
-    }
+    expect(g.error).toBe(0)
   })
 
-  it('Gameboard should place word "OSWEGO" vertically from (2,2) to (2, 7)', () => {
-    const g = new GB(11)
+  it('Gameboard should place word "OSWEGO" vertically from (5,5) to (5, 10)', () => {
+    const g = new GB()
 
     g.init()
 
     const word = 'OSWEGO'
-    const x = 2
-    const y = 2
     const h = false
 
-    g.consumeInput(x, y, h, word)
+    g.placeWord({x: 5, y: 5}, {x: 5, y: 10}, word, h)
 
-    for (let i = x; i <= x; i++) {
-      for (let j = y; j < word.length; j++) {
-        expect(g.board[j][i].letter.toUpperCase()).toEqual(word[j - y].toUpperCase())
-      }
-    }
-  })
-
-  it('Gameboard should not place a horizontal word in the cross section of a vertical word', () => {
-    const g = new GB(11)
-
-    g.init()
-
-    // Vertical Word
-    const word = 'OSWEGO'
-    const x = 2
-    const y = 2
-    const h = false
-
-    // Horizontal Word
-    const word2 = 'BAD'
-    const x2 = 1
-    const y2 = 5
-    const h2 = true
-
-    g.consumeInput(x, y, h, word)
-    const validBoard = _.cloneDeep(g.board)
-    g.consumeInput(x2, y2, h2, word2)
-
-    for (let i = 0; i < g.board.length; i++) {
-      for (let j = 0; j < g.board[0].length; j++) {
-        expect(g.board[j][i].letter.toUpperCase()).toEqual(validBoard[j][i].letter.toUpperCase())
-      }
-    }
-  })
-
-  it('Gameboard should not place a word on top of the same word', () => {
-    const g = new GB(11)
-
-    g.init()
-
-    // Vertical Word
-    const word = 'DOGE'
-    const x = 0
-    const y = 0
-    const h = true
-
-    // Horizontal Word
-    const word2 = 'DOGE'
-    const x2 = 0
-    const y2 = 0
-    const h2 = true
-
-    g.consumeInput(x, y, h, word)
-    const result = g.consumeInput(x2, y2, h2, word2)
-
-    expect(result).toEqual({
-      reason: 'Word: ' + word + ' placed on top of same word: ' + word,
-      word: word
-    })
+    expect(g.error).toBe(0)
   })
 
   it('Gameboard should place a horizontal word in the cross section of a vertical word', () => {
-    const g = new GB(11)
+    const g = new GB()
 
     g.init()
 
-    // Vertical Word
     const word = 'OSWEGO'
-    const x = 2
-    const y = 2
     const h = false
 
-    // Horizontal Word
+    g.placeWord({x: 5, y: 5}, {x: 5, y: 10}, word, h)
+
     const word2 = 'BED'
-    const x2 = 1
-    const y2 = 5
     const h2 = true
 
-    g.consumeInput(x, y, h, word)
-    g.consumeInput(x2, y2, h2, word2)
+    g.placeWord({x: 4, y: 8}, {x: 6, y: 8}, word2, h2)
 
-    for (let i = x2; i < word2.length; i++) {
-      for (let j = y2; j <= y2; j++) {
-        expect(g.board[j][i].letter.toUpperCase()).toEqual(word2[i - x2])
-      }
-    }
+    expect(g.error).toBe(0)
   })
 
-  it('Gameboard should not place a vertical word in the cross section of a horizontal word', () => {
-    const g = new GB(11)
+  it('Gameboard should not place a horizontal word in the cross section of a vertical word', () => {
+    const g = new GB()
 
     g.init()
 
-    // Horitontal Word
-    const word = 'BAD'
-    const x = 1
-    const y = 5
-    const h = true
+    const word = 'OSWEGO'
+    const h = false
 
-    // Vertical Word
-    const word2 = 'OSWEGO'
-    const x2 = 2
-    const y2 = 2
-    const h2 = false
+    g.placeWord({x: 5, y: 5}, {x: 5, y: 10}, word, h)
 
-    g.consumeInput(x, y, h, word)
-    const validBoard = _.cloneDeep(g.board)
-    g.consumeInput(x2, y2, h2, word2)
+    const word2 = 'BAD'
+    const h2 = true
 
-    for (let i = 0; i < g.board.length; i++) {
-      for (let j = 0; j < g.board[0].length; j++) {
-        expect(g.board[j][i].letter.toUpperCase()).toEqual(validBoard[j][i].letter.toUpperCase())
-      }
-    }
+    g.placeWord({x: 4, y: 8}, {x: 6, y: 8}, word2, h2)
+
+    expect(g.error).toBe(3)
   })
 
   it('Gameboard should place a vertical word in the cross section of a horizontal word', () => {
-    const g = new GB(11)
+    const g = new GB()
 
     g.init()
 
-    // Horizontal Word
     const word = 'BED'
-    const x = 1
-    const y = 5
     const h = true
 
-    // Vertical Word
+    g.placeWord({x: 4, y: 5}, {x: 6, y: 5}, word, h)
+
     const word2 = 'OSWEGO'
-    const x2 = 2
-    const y2 = 2
     const h2 = false
 
-    g.consumeInput(x, y, h, word)
-    g.consumeInput(x2, y2, h2, word2)
+    g.placeWord({x: 5, y: 2}, {x: 5, y: 7}, word2, h2)
 
-    for (let i = x2; i <= x2; i++) {
-      for (let j = y2; j < word2.length; j++) {
-        expect(g.board[j][i].letter.toUpperCase()).toEqual(word2[j - y2])
-      }
-    }
+    expect(g.error).toBe(0)
   })
 
-  it('Gameboard should return error trying to place an invalid word', () => {
-    const g = new GB(11)
+  it('Gameboard should not place a vertical word in the cross section of a horizontal word', () => {
+    const g = new GB()
 
     g.init()
 
-    const word = ''
-    const x = 9
-    const y = 5
+    const word = 'BED'
     const h = true
 
-    const result = g.consumeInput(x, y, h, word)
+    g.placeWord({x: 4, y: 5}, {x: 6, y: 5}, word, h)
 
-    expect(result).toEqual({
-      reason: 'Not a valid word',
-      word: word
-    })
+    const word2 = 'OSWAGO'
+    const h2 = false
+
+    g.placeWord({x: 5, y: 2}, {x: 5, y: 7}, word2, h2)
+
+    expect(g.error).toBe(3)
   })
 
   it('Gameboard should return error trying to place a word outside the bounds of the board', () => {
-    const g = new GB(11)
+    const g = new GB()
 
     g.init()
 
     const word = 'BED'
-    const x = 9
-    const y = 5
     const h = true
 
-    const result = g.consumeInput(x, y, h, word)
+    g.placeWord({x: 9, y: 9}, {x: 11, y: 9}, word, h)
 
-    expect(result).toEqual({
-      reason: 'Placed out of the bounds of the board',
-      word: word
-    })
+    expect(g.error).toBe(2)
+  })
+
+  it('Gamboard should place first word over the center tile', () => {
+    const g = new GB()
+
+    g.init()
+
+    const word = 'BED'
+    const h = false
+
+    g.placeWord({x: 5, y: 5}, {x: 5, y: 7}, word, h)
+
+    expect(g.error).toBe(0)
+  })
+
+  it('Gamboard should not place first word anywhere but over the center tile', () => {
+    const g = new GB()
+
+    g.init()
+
+    const word = 'BED'
+    const h = false
+
+    g.placeWord({x: 4, y: 5}, {x: 4, y: 7}, word, h)
+
+    expect(g.error).toBe(4)
+  })
+
+  it('Gamboard should place a word that is attached to previously played words', () => {
+    const g = new GB()
+
+    g.init()
+
+    const word = 'BED'
+    const h = true
+
+    g.placeWord({x: 4, y: 5}, {x: 6, y: 5}, word, h)
+
+    const word2 = 'OSWEGO'
+    const h2 = false
+
+    g.placeWord({x: 5, y: 2}, {x: 5, y: 7}, word2, h2)
+
+    expect(g.error).toBe(0)
+  })
+
+  it('Gamboard should not place a word that is not attached to previously played words', () => {
+    const g = new GB()
+
+    g.init()
+
+    const word = 'BED'
+    const h = true
+
+    g.placeWord({x: 4, y: 5}, {x: 6, y: 5}, word, h)
+
+    const word2 = 'OSWEGO'
+    const h2 = false
+
+    g.placeWord({x: 1, y: 2}, {x: 1, y: 7}, word2, h2)
+
+    expect(g.error).toBe(5)
+  })
+
+  it('Gamboard should have an error of 0', () => {
+    const g = new GB()
+
+    g.handleResponse('Oswego')
+
+    expect(g.error).toBe(0)
+  })
+
+  it('Gamboard should have an error of 1', () => {
+    const g = new GB()
+
+    g.error = 1
+
+    g.handleResponse('Oswego')
+
+    expect(g.error).toBe(1)
+  })
+
+  it('Gamboard should have an error of 2', () => {
+    const g = new GB()
+
+    g.error = 2
+
+    g.handleResponse('Oswego')
+
+    expect(g.error).toBe(2)
+  })
+
+  it('Gamboard should have an error of 3', () => {
+    const g = new GB()
+
+    g.error = 3
+
+    g.handleResponse('Oswego')
+
+    expect(g.error).toBe(3)
+  })
+
+  it('Gamboard should have an error of 4', () => {
+    const g = new GB()
+
+    g.error = 4
+
+    g.handleResponse('Oswego')
+
+    expect(g.error).toBe(4)
+  })
+
+  it('Gamboard should have an error of 5', () => {
+    const g = new GB()
+
+    g.error = 5
+
+    g.handleResponse('Oswego')
+
+    expect(g.error).toBe(5)
   })
 })
 
