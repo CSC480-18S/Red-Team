3c3f65bbea629fabcefae7771d662ebed758a4b0
Gage Davidson <gaged97@gmail.com>
2018-03-02 17:32:34 -0500

    Added player stats endpoint

--- Server Backend/routes/stats-route.js
+++ Server Backend/routes/stats-route.js
@@ -3,19 +3,40 @@
  */
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+// const VerifyToken = require('../helpers/VerifyTokens')
 
 /**
  * This endpoint gets all the stats for the given team.
  */
-router.get('/team/:team', function(req, res, next) {
-
+router.get('/teams/:team', function(req, res, next) {
+    
 })
 
 /**
  * This endpoint gets all the stats for the given player.
  */
-router.get('/player/:player', function(req, res, next) {
+router.get('/players/:player', function(req, res, next) {
+    /* Requested player's name */
+    const name = req.params.player
 
+    /* Path to access the database */
+    const path = 'http://localhost:8080/players/search/findByUsername?username=' + name
+    
+    /* Request player from database */
+    axios.get(path).then((response) => {
+        /* get returned player array */
+        const players = response.data._embedded.players
+        
+        /* if the array is empty (if there is no such player) send empty JSON
+           else return the player */
+        if (players.length == 0)
+            res.json('{}')
+        else
+            res.send(players[0])
+    }).catch(error => {
+        console.log('Error: ' + error)
+    })
 })
 
 /**
