fb8471f2d3a671eccd4acd719f32f8e40756a7ce
Dkear <dkear@oswego.edu>
2018-03-07 16:17:23 -0500

    added score/letter DB

---
+++ Database/ScoresDB/src/main/java/com/csc480red/scoresdb/ScoreRestController.java
@@ -0,0 +1,31 @@
+package com.csc480red.scoresdb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScoreRestController {
+	@Autowired
+	ScoreRepository scoreRepository;
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "scores")
+	public ResponseEntity<?> putScore(@RequestBody Score input){
+		Score score = new Score(input.getA(), input.getB(), input.getC(), input.getD(), input.getE(), input.getF(), input.getG(), input.getH(), input.getI(), input.getJ(), input.getK(), input.getL(), input.getM(), input.getN(), input.getO(), input.getP(), input.getQ(), input.getR(), input.getS(), input.getT(), input.getU(), input.getV(), input.getW(), input.getX(), input.getY(), input.getZ());
+		scoreRepository.deleteAll();
+		scoreRepository.save(score);
+		
+		return ResponseEntity.accepted().build();
+
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "scores")
+	public Score scores() {
+		return scoreRepository.getOne(0L);
+	}
+	
+	
+}
