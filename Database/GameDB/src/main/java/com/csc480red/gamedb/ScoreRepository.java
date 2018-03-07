2e4aa82296a416e87d2c1c483899dfb7eb7ed386
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2018-03-07 15:11:52 -0500

    Revert "added letter DB"
    This reverts commit 7f9941df6424be2df84e35696b57563469ebd029.

--- Database/GameDB/src/main/java/com/csc480red/gamedb/ScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.csc480red.scoresdb;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//HERE IS THE ENDPOINT
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-
-}
