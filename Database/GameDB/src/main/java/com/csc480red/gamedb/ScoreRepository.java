26c67ba2b4e81f56033deeef3d15cfc18c7aa574
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2018-03-07 15:02:14 -0500

    Remove unnecessary comment

--- Database/GameDB/src/main/java/com/csc480red/gamedb/ScoreRepository.java
+++ Database/GameDB/src/main/java/com/csc480red/gamedb/ScoreRepository.java
@@ -2,7 +2,6 @@ package com.csc480red.scoresdb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//HERE IS THE ENDPOINT
 public interface ScoreRepository extends JpaRepository<Score, Long> {
 
 }
