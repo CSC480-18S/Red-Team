9f92dbdb3bc97fd4a0e1c137122bc187ae593612
Cesar <cesarbiods@gmail.com>
2018-03-07 23:45:55 -0500

    Add team finished
    When creating users they are now assigned a team from the start

--- Server Backend/routes/users-route.js
+++ Server Backend/routes/users-route.js
@@ -1,44 +1,45 @@
 /**
  * Imports the `express`, `router` and other modules
- * for using JWT, cookies
+ * for using JWT, cookies, and axios
  */
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
+const Axios = require('axios')
 const config = require('../config')
 const VerifyToken = require('../helpers/VerifyTokens')
 const cookieParser = require('cookie-parser')
 
 router.use(cookieParser())
 
-/**
- * Users array
- */
-var users = []
+const axios = Axios.create({baseURL: 'http://localhost:8091/'})
+
+var error = 0
 
 /**
  * Route that creates a user
  */
 router.post('/createUser', function(req, res, next) {
-  /**
-     * Create new user object
-     */
   const newUser = {
-    username: req.body.username
+    username: req.body.username,
+    team: req.body.team
   }
 
-  /**
-     * TODO check the database doesn't
-     * contain the requested username
-     */
-
-  /**
-     * Take the user's parameters and create a
-     * token for them
-     */
-  for (let i = 0; i < users.length; i++) {
-    if (newUser.username === users[i].username) {
-      res.status(500).json({code: 'U1', title: 'User error', desc: 'Username already taken'})
+  checkUserExists(newUser).then(response => {
+    if (response.length > 0) {
+      error = 1
+      return handleErrors(newUser.username)
+    }
+    return addUser(newUser)
+  }).then(response => {
+    // create a token
+    if (error === 0) {
+      let token = jwt.sign({ id: newUser._id }, config.secret, {
+        expiresIn: '1 year' // expires in 1 year
+      })
+      // save token as cookie
+      res.cookie('token', token, { expires: new Date(253402300000000) })
+      res.json({ username: newUser.username, team: newUser.team })
       return
     }
   }
@@ -51,25 +52,89 @@ router.post('/createUser', function(req, res, next) {
   var token = jwt.sign({ username: newUser.username }, config.secret, {
     expiresIn: '1 year' // expires in 1 year
   })
-  res.cookie('token', token, {expires: new Date(253402300000000)})
-
-  /**
-     * TODO add created user to database
-     */
-  users.push(newUser)
-
-  /**
-     * Returns to the user a JSON object containing the new user
-     */
-  res.json({username: newUser.username})
 })
+
+/**
+ * Helper function for adding
+ * players to the DB
+ */
+function addUser(newUser) {
+  /**
+ * Take GUIs team name and
+ * translate it to the proper
+ * URL for database
+ */
+  var teamURL
+  if (newUser.team === 'Gold') {
+    teamURL = 'http://localhost:8091/teams/1'
+  } else if (newUser.team === 'Green') {
+    teamURL = 'http://localhost:8091/teams/2'
+  } else throw error
+
+  return axios.post('players', {
+    username: newUser.username,
+    team: teamURL
+  })
+    .then(function(response) {
+      console.log('player added')
+    })
+    .catch(function(e) {
+      error = 2
+    })
+}
+
+/**
+ * Helper function for checking
+ * whether a username is already
+ * taken in the DB
+ */
+function checkUserExists(newUser) {
+  return axios.get('players/search/findByUsername?username=' + newUser.username, {
+  })
+    .then(function(response) {
+      return response.data._embedded.players
+    })
+    .catch(function(e) {
+      error = 1
+    })
+}
+
+/**
+ * Helper function for handling
+ * error messages when creating
+ * users
+ */
+function handleErrors(username) {
+  const result = {
+    invalid: true
+  }
+  let reason = null
+
+  result['username'] = username.toUpperCase()
+
+  switch (error) {
+    case 1:
+      reason = 'Username is already taken'
+      break
+    case 2:
+      reason = 'There was a problem registering the user'
+      break
+    default:
+      result.invalid = false
+      return result
+  }
+
+  result['reason'] = reason.toUpperCase()
+
+  return result
+}
 
 /**
  * Route that updates a user's score
  */
 router.put('/updateScore', VerifyToken, function(req, res, next) {
   /**
-     * The word score to be updated
+     * The word score to be updated, How????
      */
   const wscore = req.body.user
 
@@ -92,53 +157,44 @@ router.put('/updateScore', VerifyToken, function(req, res, next) {
  */
 router.get('/getUser', VerifyToken, function(req, res, next) {
   /**
-     * TODO look up the user on the databse
+     * Looks up the user on the database
      */
-  var found = null
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].username === req.query.username) {
-      found = users[i]
-    }
-  }
-
-  /**
-     * TODO returns a JSON containing better user information
-     */
-  if (!found) res.status(400).json({code: 'U3', title: 'User error', desc: 'User was not found'})
-  else {
-    res.json({success: 'You have been logged in!'})
-  }
+  axios.get('players/search/findByUsername?username=' + req.query.username, {
+  })
+    .then(function(response) {
+      if (response.data._embedded.players[0]) {
+        console.log('You have been logged in')
+        res.json(response.data._embedded.players[0])
+      } else {
+        throw error
+      }
+    })
+    .catch(function(e) {
+      res.status(400).json({code: 'U3', title: 'User error', desc: 'User was not found'})
+    })
 })
 
 /**
  * Route that returns the user's team
  */
 router.get('/currentTeam', VerifyToken, function(req, res, next) {
-
   /**
-     * TODO look up the user on the databse
+     * Looks up the user on the database
      */
-
+  axios.get('players/search/findByUsername?username=' + req.query.username, {
+  })
+    .then(function(response) {
+      if (response.data._embedded.players[0]._links.team.href) {
+        res.json(response.data._embedded.players[0]._links.team.href)
+      } else {
+        throw error
+      }
+    })
+    .catch(function(e) {
+      res.status(400).json({code: 'U4', title: 'Server error', desc: 'Something went wrong'})
+    })
   /**
      * TODO returns a String for the user's team
-     */
-})
-
-/**
- * Route that sets the user's team manually
- */
-router.put('/setTeam', VerifyToken, function(req, res, next) {
-  /**
-     * The team the user will be assigned to
-     */
-  const team = req.body.user
-
-  /**
-     * TODO update user's team on the database
-     */
-
-  /**
-     * TODO Returns a success/fail boolean
      */
 })
 
@@ -163,9 +219,14 @@ router.get('/allUsers', function(req, res, next) {
   /**
      * Sends the user a JSON object containing all of the users
      */
-  res.json({
-    all_users: users
+  axios.get('players', {
   })
+    .then(function(response) {
+      res.json(response.data._embedded.players)
+    })
+    .catch(function(e) {
+      res.status(500).json({code: 'U4', title: 'Server error', desc: 'Something went wrong'})
+    })
 })
 
 /**
