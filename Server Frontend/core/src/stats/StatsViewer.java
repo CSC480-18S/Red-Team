e783d308a67470db2bc2fb0f358b2778f6876a03
Cesar <cesarbiods@gmail.com>
2018-03-31 20:05:24 -0400

    Got userStats connection working
    Was able to print out user stats to stdout

--- Server Frontend/core/src/stats/StatsViewer.java
+++ Server Frontend/core/src/stats/StatsViewer.java
@@ -3,10 +3,20 @@ package stats;
 import com.badlogic.gdx.Game;
 import stats.GUI.StatsScreen;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
 public class StatsViewer extends Game {
     public StatsScreen screen;
     @Override
     public void create() {
+        StatsConnection stats = new StatsConnection();
+        try (Scanner scanner = new Scanner(stats.getUserStats("Gage"))) {
+            String responseBody = scanner.useDelimiter("\\A").next();
+            System.out.println(responseBody);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         screen = new StatsScreen(this);
         setScreen(screen);
     }
