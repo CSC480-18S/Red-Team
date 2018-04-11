a03ac72922c49d6f503f24ffb87dc407a7e7d839
cking20 <cking20@oswego.edu>
2018-04-10 20:05:23 -0400

    command line arguments, event rename

--- Server Frontend/desktop/src/com/csc480/game/desktop/DesktopLauncher.java
+++ Server Frontend/desktop/src/com/csc480/game/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package com.csc480.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csc480.game.Engine.GameManager;
 import com.csc480.game.GameAdapter;
 import stats.StatsAdapter;
 
@@ -9,15 +10,25 @@ import stats.StatsAdapter;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		boolean runStatsFlag = false;
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		//config.fullscreen = true;
 		config.height = 1080;
 		config.width = 1920;
-		if(arg.length == 0)
+		if(arg.length == 0) {
 			new LwjglApplication(new GameAdapter(), config);
-		else if(arg[0].compareTo("-stats") == 0)
-			new LwjglApplication(new StatsAdapter(),config);
-
+		}else {
+			for(int i = 0; i < arg.length; i++) {
+				if (arg[i].compareTo("-stats") == 0)
+					runStatsFlag = true;
+				if (arg[i].compareTo("-ai") == 0)
+					GameManager.produceAI = true;
+			}
+		}
+		if(runStatsFlag)
+			new LwjglApplication(new StatsAdapter(), config);
+		else
+			new LwjglApplication(new GameAdapter(), config);
 
 	}
 }
