e8d75cfc29b0cee2d41c91738e41905fa5ade9a6
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2018-04-16 00:35:57 -0400

    Automatically compute statistics

--- Database/GameDB/src/main/java/com/csc480red/gamedb/PlayedWord.java
+++ Database/GameDB/src/main/java/com/csc480red/gamedb/PlayedWord.java
@@ -4,9 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
 
 @Entity
+@Getter
 public class PlayedWord {
 	
 	@Id
@@ -15,122 +19,21 @@ public class PlayedWord {
 	
 	private String word;
 	private int value;
-	private int length;
-	private boolean standalone;
-	private boolean dw;
-	private boolean tw;
-	private boolean dl;
-	private boolean tl;
+	private boolean dirty;
+	private boolean special;
 	
-	@OneToOne
+	@ManyToOne
+	@JoinColumn(name="player_id")
 	private Player player;
 	
 	protected PlayedWord() {}
 
-	/**
-	 *
-	 * @param word - the word that was played
-	 * @param value - the value of the played word
-	 * @param length = the length of the played word
-	 * @param standalone - is the word a standalone word
-	 * @param dw - was the word played over a double word tile
-	 * @param tw - was the word played over a triple word tile
-	 * @param dl - was the word played over a double letter tile
-	 * @param tl - was the word played over a triple letter tile
-	 * @param player - the player that played the word
-	 */
-	public PlayedWord(String word, int value, int length, boolean standalone, boolean dw, boolean tw, boolean dl,
-			boolean tl, Player player) {
+	public PlayedWord(String word, int value, boolean dirty, boolean special, Player player) {
 		super();
 		this.word = word;
 		this.value = value;
-		this.length = length;
-		this.standalone = standalone;
-		this.dw = dw;
-		this.tw = tw;
-		this.dl = dl;
-		this.tl = tl;
+		this.dirty = dirty;
+		this.special = special;
 		this.player = player;
 	}
-
-	/**
-	 *
-	 * @return id
-	 */
-	public Long getId() {
-		return id;
-	}
-
-	/**
-	 *
-	 * @return the actual word
-	 */
-	public String getWord() {
-		return word;
-	}
-
-	/**
-	 *
-	 * @return value
-	 */
-	public int getValue() {
-		return value;
-	}
-
-	/**
-	 *
-	 * @return length
-	 */
-	public int getLength() {
-		return length;
-	}
-
-	/**
-	 *
-	 * @return is it standalone
-	 */
-	public boolean isStandalone() {
-		return standalone;
-	}
-
-	/**
-	 *
-	 * @return over a double word tile
-	 */
-	public boolean isDw() {
-		return dw;
-	}
-
-	/**
-	 *
-	 * @return over a triple word tile
-	 */
-	public boolean isTw() {
-		return tw;
-	}
-
-	/**
-	 *
-	 * @return over a double letter tile
-	 */
-	public boolean isDl() {
-		return dl;
-	}
-
-	/**
-	 *
-	 * @return over a triple letter tile
-	 */
-	public boolean isTl() {
-		return tl;
-	}
-
-	/**
-	 *
-	 * @return player that played this word
-	 */
-	public Player getPlayer() {
-		return player;
-	}
-
 }
---
+++ Database/GameDB/src/main/java/com/csc480red/gamedb/WordFrequency.java
@@ -0,0 +1,9 @@
+package com.csc480red.gamedb;
+
+import lombok.Data;
+
+@Data
+public class WordFrequency {
+	private final String word;
+	private final int frequency;
+}
