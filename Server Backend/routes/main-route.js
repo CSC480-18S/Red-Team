6e121bd97976cfc2dc84cc7e79fabb1110e1a34a
Landon Patmore <landon.patmore@gmail.com>
2018-04-18 14:46:35 -0400

    updating

--- Server Backend/routes/main-route.js
+++ Server Backend/routes/main-route.js
@@ -4,37 +4,50 @@
 const express = require('express')
 const router = express.Router()
 const mg = require('../helpers/MacGrabber')
+const session = require('express-session')
+
+let ses = session({
+  name: 'login-session',
+  secret: 'testlol'
+})
 
 module.exports = (socket) => {
 /* eslint no-new:0 */
   const GameManager = require('../entities/GameManager')(socket)
-  new GameManager()
+  const g = new GameManager()
 
   let users = []
 
-  router.get('/', function(req, res, next) {
+  router.get('/', ses, function(req, res, next) {
     let ip = req.ip
     ip = ip.split(':')[3]
     for (let i = 0; i < users.length; i++) {
       if (users[i].mac === mg(ip)) {
-        res.render('login', {user: users[i]})
+        req.session.user = users[i].username
+        res.render('login', {user: users[i], error: req.session.error})
         return
       }
     }
 
-    res.render('register')
+    res.render('register', {error: req.session.error})
   })
 
   router.get('/admin', function(req, res, next) {
     res.render('admin')
   })
 
-  router.get('/game', function(req, res, next) {
-    res.render('gameboard')
+  router.get('/game', ses, function(req, res, next) {
+    console.log(req.session)
+    if (!req.session.user) {
+      req.session.error = 'Please register/login first.'
+      res.redirect('/')
+    } else {
+      res.render('gameboard')
+      req.session.destroy()
+    }
   })
 
-  router.post('/register', function(req, res, next) {
-    console.log(req.body)
+  router.post('/register', ses, function(req, res, next) {
     let ip = req.ip
     ip = ip.split(':')[3]
     let user = {
@@ -44,9 +57,15 @@ module.exports = (socket) => {
     }
 
     users.push(user)
+    req.session.error = undefined
 
     res.redirect('/')
   })
+
+  function gateCheck(req, res, next) {
+    console.log(req.bob === true)
+    next()
+  }
 
   return router
 }
