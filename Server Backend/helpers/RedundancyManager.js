af7018b59aca9cae6e7f54a5f8fb7064fb54540a
Gage Davidson <gaged97@gmail.com>
2018-04-22 20:38:12 -0400

    Extract file I/O to FileManager

--- Server Backend/helpers/RedundancyManager.js
+++ Server Backend/helpers/RedundancyManager.js
@@ -1,85 +1,65 @@
 'use strict'
 const fs = require('fs')
 const axios = require('axios')
+const FileManager = require('./FileManager')
 
 const logsDir = './logs'
-const logFile = logsDir + '/redundancyManager.log'
+const logFile = 'redundancyManager.log'
 
-let savedData = []
-
-/**
- * Adds an item to be resent later.
- */
-function saveForLater(url, data) {
-  savedData.push({
-    url: url,
-    data: data
-  })
-
-  writeFile()
-}
-
-/**
- * Creates the log dir if necessary and loads the log.
- */
-function init() {
-  fs.exists(logsDir, function(exists) {
-    if (!exists) {
-      fs.mkdir(logsDir, function(err) {
-        if (err) {
-          console.log('Error while making dir, ', err)
-        }
-      })
-    }
-  })
-
-  fs.readFile(logFile, (err, data) => {
-    if (err) {
-      console.log('Error while reading file')
-    } else {
-      savedData = JSON.parse(data)
-      resend()
-    }
-  })
-}
-
-/**
- * Attempts to resend the saved data.
- */
-function resend() {
-  let changes = false
-
-  for (let i = 0; i < savedData.length; i++) {
-    axios.post(savedData[i].url, savedData[i].data)
-      .then(function(response) {
-        savedData.splice(i, 1)
-        i--
-        changes = true
-      })
-      .catch(function(e) {
-      // failed, do nothing
-      })
+class RedundancyManager {
+  
+  constructor() {
+    this._savedData = []
+    this._rawData = ''
+    this._fm = new FileManager(logsDir, logFile)
+    this._fm.readFile()
   }
+  
+  /**
+   * Adds an item to be resent later.
+   */
+  saveForLater(url, data) {
+    this._savedData.push({
+      url: url,
+      data: data
+    })
+    
+    this._fm.writeFile(JSON.stringify(this._savedData))
+  }
+  
+  /**
+   * Loads the log.
+   */
+  loadLog() {
+    this._savedData = JSON.parse(this._fm.data)
+  }
+  
+  /**
+   * Attempts to resend the saved data.
+   */
+  resend() {
+    let changes = false
 
-  if (changes) {
-    writeFile()
+    for (let i = 0; i < this._savedData.length; i++) {
+      axios.post(this._savedData[i].url, this._savedData[i].data)
+        .then(function(response) {
+          this._savedData.splice(i, 1)
+          i--
+          changes = true
+        })
+        .catch(function(e) {
+          // failed, do nothing
+        })
+    }
+
+    if (changes) {
+      let json = JSON.stringify(this._savedData)
+      this._fm.writeFile(json)
+    }
   }
 }
 
 /**
- * Writes the savedData array to the log file.
+ * Exports this file so it can be used by other files.  Keep this at the bottom.
  */
-function writeFile() {
-  fs.writeFile(logFile, JSON.stringify(savedData), function(err) {
-    if (err) {
-      console.log('Error while writing file, ', err)
-    } else {
-      console.log('Data saved')
-    }
-  })
-}
-
-module.exports = {
-  saveForLater,
-  init
-}
+module.exports = RedundancyManager
--- Server Backend/server.js
+++ Server Backend/server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const http = require('http')
-const rm = require('./helpers/RedundancyManager')
+const RedundancyManager = require('./helpers/RedundancyManager')
 
 /**
  * Set to a variable, instead of typing this out everytime
@@ -100,4 +100,10 @@ server.listen(port, function() {
   console.log('Server started on port ' + port)
 })
 
-rm.init()
+const rm = new RedundancyManager()
+setTimeout(function() {
+  rm.loadLog()
+}, 1000)
+setTimeout(function() {
+  rm.resend()
+}, 2000)
