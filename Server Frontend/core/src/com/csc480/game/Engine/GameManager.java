23a90631933609af970fe746d82055561a86198e
james-aric <jschalle@oswego.edu>
2018-04-29 14:58:17 -0400

    removed socketio from GameManager

--- Server Frontend/core/src/com/csc480/game/Engine/GameManager.java
+++ Server Frontend/core/src/com/csc480/game/Engine/GameManager.java
@@ -6,10 +6,6 @@ import com.badlogic.gdx.utils.Array;
 import com.csc480.game.Engine.Model.*;
 import com.csc480.game.GUI.GameScreen;
 import com.csc480.game.OswebbleGame;
-import io.socket.client.IO;
-import io.socket.emitter.Emitter;
-import io.socket.client.Socket;
-import org.java_websocket.WebSocket;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONArray;
@@ -41,8 +37,6 @@ public class GameManager {
     public int greenScore;
     public int goldScore;
     public boolean gameOver;
-    //socket stuff
-    private Socket socket;
     private double reconnectTimer = 2000.0;
     private ArrayList<String> eventBacklog;
 
@@ -72,8 +66,6 @@ public class GameManager {
         placementsUnderConsideration = new ArrayList<Placement>();
         theBoard = new Board(OswebbleGame.BOARD_SIZE);
         eventBacklog = new ArrayList<String>();
-        //SocketManager.getInstance().ConnectSocket();
-        //SocketManager.getInstance().setUpEvents();
         WordVerification.getInstance();
         ConnectSocket();
         connection.connect();
@@ -116,11 +108,6 @@ public class GameManager {
         ApplyEventBackLog();
     }
 
-    public void Dispose(){
-        if(socket != null)
-            socket.disconnect();
-        TextureManager.getInstance().Dispose();
-    }
 
     ///////Socket Stuff
     /**
@@ -128,11 +115,6 @@ public class GameManager {
      */
     public void ConnectSocket(){
         try {
-            /*if(socket != null)
-                socket.disconnect();
-            socket = null;
-            socket = IO.socket("http://localhost:3000");
-            socket.connect();*/
             connection = new WebSocketClient(new URI("ws://localhost:3000")) {
                 @Override
                 public void onOpen(ServerHandshake handshakedata) {
@@ -392,349 +374,13 @@ public class GameManager {
             System.err.println(e);
         }
     }
-/*    public void ReConnectSocket(){
-        try {
-            IO.Options opts = new IO.Options();
-            opts.forceNew = true;
-            opts.reconnection = true;
-            socket = IO.socket("http://localhost:3000", opts);
-            socket.connect();
-        } catch (URISyntaxException e){
-            System.err.println(e);
-        }
-    }*/
-
-
-    /**
-     * Define the actions to be taken when events occur
-     */
-    /*
-    public void setUpEvents(){
-        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                LogEvent("connected to the backend");
-                //simple example of how to access the data sent from the server
-                try {
-                    //JSONObject data = (JSONObject) args[0];
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("whoAreYou", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-//                LogEvent("whoAreYou");
-                JSONObject data = new JSONObject();
-                data.put("isSF",true);
-                socket.emit("whoAreYou",data);
-            }
-        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                LogEvent("disconnection");
-                System.out.println("attempting reconnection:");
-                if(debug)
-                    theGame.theGameScreen.debug.setText("Got disconnect. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-                ReConnectSocket();
-
-            }
-        }).on(Socket.EVENT_RECONNECTING, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("reconnecting");
-            }
-        }).on(Socket.EVENT_RECONNECT_ATTEMPT, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("reconnect attempt");
-            }
-        }).on(Socket.EVENT_RECONNECT_FAILED, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("reconnect failed");
-            }
-        }).on(Socket.EVENT_CONNECT_TIMEOUT, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("connect timeout");
-            }
-        }).on(Socket.EVENT_RECONNECT, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("reconnect");
-                for(int i = 0; i < 4; i++){
-                    if(produceAI) {
-                        theAIs[i] = new AI();
-                        thePlayers[i] = theAIs[i];
-                    }else {
-                        thePlayers[i] = new Player();
-                    }
-                }
-            }
-        }).on("removeAI", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("frontend got removeAI");
-                try {
-                    JSONObject data = (JSONObject) args[0];
-                    int position = data.getInt("position");
-                    if(theAIs[position] != null)
-                        theAIs[position].disconnectAI();
-                    theAIs[position] = null;
-                    thePlayers[position] = new Player();
-
-                    if(debug)
-                        theGame.theGameScreen.debug.setText("Got removeAI. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("connectAI", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                LogEvent("Reconnecting an AI");
-                System.out.println("connectAI");
-                try {
-                    JSONObject data = (JSONObject) args[0];
-                    System.out.println(data.toString());
-                    int position = data.getInt("position");
-                    //reconnect an AI
-                    theAIs[position] = new AI();
-                    thePlayers[position] = theAIs[position];
-                    switch (position){
-                        case 0:
-                            theGame.theGameScreen.bottom.setPlayer(theAIs[(position)]);
-                            theGame.theGameScreen.bottom.updateState();
-                            break;
-                        case 1:
-                            theGame.theGameScreen.right.setPlayer(theAIs[(position)]);
-                            theGame.theGameScreen.right.updateState();
-                            break;
-                        case 2:
-                            theGame.theGameScreen.top.setPlayer(theAIs[(position)]);
-                            theGame.theGameScreen.top.updateState();
-                            break;
-                        case 3:
-                            theGame.theGameScreen.left.setPlayer(theAIs[(position)]);
-                            theGame.theGameScreen.left.updateState();
-                            break;
-                    }
-                    thePlayers[position] = theAIs[(position)];
-                    if(debug)
-                        theGame.theGameScreen.debug.setText("Got connectAI. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("boardUpdate", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-//                LogEvent("boardUpdate");
-                System.out.println("frontend got boardUpdate");
-                try {
-                    JSONObject data = (JSONObject) args[0];
-                    System.out.println("data: "+data.toString());
-                    JSONArray board = data.getJSONArray("board");
-                    try{
-                        int greenScore = data.getInt("green");
-                        int yellowScore = data.getInt("gold");
-                    }catch (JSONException e){
-                        System.out.println("ERROR: team names messed up");
-
-                    }
-                    //find the board/user state differences
-                    wordHasBeenPlayed(unJSONifyBackendBoard(board));
-                    //hard update the game and user states
-                    hardUpdateBoardState(unJSONifyBackendBoard(board));
-                    if(debug)
-                        theGame.theGameScreen.debug.setText("Got boardUpdate. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("gameEvent", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("frontend got gameEvent");
-                try {
-                    JSONObject data = (JSONObject) args[0];
-                    boolean isBonus = false;
-                    String action = data.getString("action");
-                    if(data.get("bonus") != JSONObject.NULL)
-                        isBonus = data.getBoolean("bonus");
-                    //System.out.println(action);
-                    if(isBonus)
-                        BonusEvent(action);
-                    LogEvent(action);
-                    if(debug)
-                        theGame.theGameScreen.debug.setText("Got gameEvent. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("updateState", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("frontend got updateState");
-                try {
-                    JSONObject data = (JSONObject) args[0];
-                    System.out.println(data);
-                    JSONArray players = data.getJSONArray("players");
-                    for(int i = 0; i < players.length(); i++){
-                        JSONObject player  = (JSONObject)players.get(i);
-                        int index = player.getInt("position");
-                        boolean isAI;
-                        try {
-                            isAI = player.getBoolean("isAI");
-                        }catch(JSONException e){
-                            //the
-                            isAI = true;
-                            //todo reconnect an AI at that position
-                            theAIs[index].disconnectAI();
-                            theAIs[index] = null;
-                            theAIs[index] = new AI();
-                            thePlayers[index] = theAIs[index];
-                        }
-                        try {
-                            if(player.get("score") != JSONObject.NULL)
-                                thePlayers[index].score = player.getInt("score");
-                            else
-                                thePlayers[index].score = 0;
-
-                            if(player.get("name") != JSONObject.NULL)
-                                thePlayers[index].name = player.getString("name");
-                            else
-                                thePlayers[index].name = "";
-
-                            if(player.get("team") != JSONObject.NULL)
-                                thePlayers[index].team = player.getString("team");
-                            else
-                                thePlayers[index].team = "";
-
-                            if(player.get("tiles") != JSONObject.NULL){
-                                JSONArray hand = player.getJSONArray("tiles");
-                                for(int h = 0; h < hand.length(); h++){
-                                    if(isAI)
-                                        thePlayers[index].tiles[h] = hand.getString(h).toLowerCase().charAt(0);
-                                    else
-                                        thePlayers[index].tiles[h] = '_';
-                                }
-                            }
-
-                            thePlayers[index].turn = player.getBoolean("isTurn");
-                            System.out.println("updating player @ index " + index);
-                        }catch (JSONException e){
-                            e.printStackTrace();
-                        }
-                        /*
-                        JSONArray hand = player.getJSONArray("hand");
-                        for(int j = 0; j < hand.length(); j++){
-                            thePlayers[index].tiles[j] = hand.getString(j).charAt(0);
-                        }
-
-                    }
-                    if(theGame!= null){
-                        if(theGame.theGameScreen != null)
-                            theGame.theGameScreen.QueueUpdate();
-                    }
-                    if(debug)
-                        theGame.theGameScreen.debug.setText("Got updateState. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("gameOver", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("gameOver");
-                LogEvent("gameOver");
-                //"score": scores of game "timeout": double
-                //"gameData": array of all data of the scores and such
-                try {
-                    JSONObject data = (JSONObject) args[0];
-                    System.out.println(data.toString());
-                    JSONArray scores = data.getJSONArray("scores");
-                    Array<String> playersScores = new Array<String>();
-                    for(int i = 0; i < scores.length(); i++){
-                        JSONObject j = (JSONObject) scores.get(i);
-                        playersScores.add(j.getString("name")+" scored "+j.getInt("score")+ " points!");
-                    }
-
-                    String winner = "Congratulations, Everyone!!!";
-                    if(data.get("winner") != JSONObject.NULL)
-                           winner = "Congratulations, "+data.getString("winner")+"!!!";
-                    String winningTeam = "No one";
-                    if(data.get("winningTeam") != JSONObject.NULL)
-                        winningTeam = data.getString("winningTeam");
-
-                    //todo call @GUI stuff
-                    theGame.theGameScreen.gameOverActor.update(winner, playersScores, winningTeam);
-                    theGame.theGameScreen.gameOverActor.setVisible(true);
-                    if(debug)
-                        theGame.theGameScreen.debug.setText("Got gameOverEvent. Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START));
-                }catch(ArrayIndexOutOfBoundsException e){
-                    e.printStackTrace();
-                }catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }).on("newGame", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("newGame");
-                theGame.theGameScreen.gameOverActor.setVisible(false);
-                if(debug)
-                    theGame.theGameScreen.debug.setText("Game num: "+(theGame.theGameScreen.NUM_GAMES_SINCE_START++));
-            }
-        }).on("newGameCountdown", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                System.out.println("ferver frontend got newGameCountdown");
-                JSONObject data = (JSONObject) args[0];
-                int t = 20;
-                if(data.get("time") != JSONObject.NULL)
-                    t = data.getInt("time");
-                theGame.theGameScreen.gameOverActor.updateTime(t);
-//                theGame.theGameScreen.gameOverActor.setVisible(false);
-            }
-        });
-    }*/
-
+    
     private TileData[][] parseServerBoard(JSONArray board){
         TileData[][] parsed = new TileData[board.length()][board.length()];
         JSONArray col;
         for(int i = 0; i < board.length(); i++){
             col = board.getJSONArray(i);
             for(int j = 0; j < col.length(); j++){
-                /*//the old, proper way to do it
-                JSONObject serverTile = col.getJSONObject(j);
-                //int x, int y, char the_letter, int value, int bonus, String player, long timePlayed
-                if(serverTile.getBoolean("_letterPlaced ")){
-                    TileData t = new TileData(
-                        serverTile.getInt("_x"),
-                        10-serverTile.getInt("_y"),
-                        serverTile.getString("_letter").charAt(0),
-                        0,
-                        serverTile.getInt("_multiplier"),
-                        serverTile.getString("_playedBy"),
-                        serverTile.getLong("_timePlayedAt")
-                    );
-                parsed[i][j] = t;}
-                */
                 //the hacky way because the server is sending Strings
                 String serverTile = col.getString(j);
                 if(serverTile.compareTo("null") == 0) {
