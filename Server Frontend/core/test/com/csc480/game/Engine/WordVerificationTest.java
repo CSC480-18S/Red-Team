e38b4d5781db38f89b564674406880374fa760d2
james-aric <jschalle@oswego.edu>
2018-04-29 16:59:50 -0400

    added libgdx compatible junit testing

---
+++ Server Frontend/core/test/com/csc480/game/Engine/WordVerificationTest.java
@@ -0,0 +1,32 @@
+package com.csc480.game.Engine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordVerificationTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void TESTgetWordsFromHand() {
+    }
+
+    @Test
+    public void isWord() {
+    }
+
+    @Test
+    public void genRegexOld() {
+    }
+
+    @Test
+    public void genRegex() {
+    }
+
+    @Test
+    public void printPlay() {
+    }
+}
